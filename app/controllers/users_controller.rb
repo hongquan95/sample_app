d37b51f4c8918712ee1b7c982268a9928e79390a
hongquan95 <hongquan95.tran@gmail.com>
2018-02-08 17:17:00 +0700

    Implement basic login-Chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find params[:id]
+    @user = User.find_by id: params[:id]
   end
 
   def new
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "users.create.welcome"
       redirect_to @user
     else
