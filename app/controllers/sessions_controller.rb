470c66ac3e4005e57fec2d513e88c21490d14122
hongquan95 <hongquan95.tran@gmail.com>
2018-02-08 17:17:00 +0700

    Add account activation-Chapter11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      excute_after_check_activated
     else
       flash.now[:danger] = t "sessions.create.invalid"
       render :new
@@ -16,6 +14,19 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def excute_after_check_activated
+    if @user.activated?
+      log_in @user
+      params[:session][:remember_me] == Settings.checked ? remember(@user) : forget(@user)
+      redirect_back_or @user
+    else
+      flash[:warning] = t ".notice"
+      redirect_to root_url
+    end
   end
 
   def load_user
